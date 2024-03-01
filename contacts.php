0dc5f6519015045cf97e3eb73716ae5ef1084b7d
apo2810 <apolline.fert@gmail.com>
2024-03-01 12:29:53 +0100

    page ref + changement nav

--- contacts.php
+++ contacts.php
@@ -34,29 +34,30 @@
 </head>
 
 <body>
-  <header id="header" class="header d-flex align-items-center sticked stikcy-menu">
-      <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
-          <img src="assets/images/fses-blc.png" alt="logo fses">
-        </a>
-        <nav id="navbar" class="navbar">
-          <ul>
-            <li><a href="index.html" class="">Accueil</a></li>
-            <li class="dropdown"><a href="services.html"><span>Services</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-              <ul>
-                <li><a href="services.html#telesurveillance">Telesurveillance</a></li>
-                <li><a href="services.html#intervention">Intervention</a></li>
-                <li><a href="services.html#gardiennage">Gardiennage</a></li>
-                <li><a href="services.html#rondes">Rondes</a></li>
-              </ul>
-            </li>
-            <li><a href="recrutement.html" class="">Recrutement</a></li>
-          </ul>
-        </nav><!-- .navbar -->
-        <a href="contact.html" class="btn-get-started hide-on-mobile">Nous contacter</a>
-        <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-        <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-      </div>
+<header id="header" class="header d-flex align-items-center sticked stikcy-menu">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/images/fses-blc.png" alt="logo fses">
+      </a>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.html" class="">Accueil</a></li>
+          <li class="dropdown"><a href="services.html"><span>Services</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="services.html#telesurveillance">Telesurveillance</a></li>
+              <li><a href="services.html#intervention">Intervention</a></li>
+              <li><a href="services.html#gardiennage">Gardiennage</a></li>
+              <li><a href="services.html#rondes">Rondes</a></li>
+            </ul>
+          </li>
+          <li><a href="recrutement.html" class="">Recrutement</a></li>
+          <li><a href="reference.html" class="">Références</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+      <a href="contact.html" class="btn-get-started hide-on-mobile">Nous contacter</a>
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+    </div>
   </header>
   <!-- End Header -->
 
